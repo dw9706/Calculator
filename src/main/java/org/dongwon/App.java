441b19a3b5610da303383eaf158c99ab671b6b36
kodongwon <dw9706@gmail.com>
2025-04-17 11:34:51 +0900

    feat: Lv2. 결과 출력 기능 구현
    - 연산 후 반환 받은 값을 출력.
    - 0으로 나눌 때 예외 처리.

--- src/main/java/org/dongwon/App.java
+++ src/main/java/org/dongwon/App.java
@@ -7,18 +7,23 @@ public class App {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = new Calculator();
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            // nextInt()를 호출하면 개행문자가 남아 String을 받아 Integer로 파싱.
-            int num1 = Integer.parseInt(sc.nextLine());
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                // nextInt()를 호출하면 개행문자가 남아 String을 받아 Integer로 파싱.
+                int num1 = Integer.parseInt(sc.nextLine());
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = Integer.parseInt(sc.nextLine());
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = Integer.parseInt(sc.nextLine());
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operation = sc.nextLine().trim().charAt(0);
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operation = sc.nextLine().trim().charAt(0);
 
-            int result = calculator.calculate(num1, num2, operation);
-            System.out.println("결과: " + result);
+                int result = calculator.calculate(num1, num2, operation);
+                System.out.println("결과: " + result);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
 
             System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
             String exit = sc.nextLine().trim();
--- src/main/java/org/dongwon/Calculator.java
+++ src/main/java/org/dongwon/Calculator.java
@@ -1,6 +1,7 @@
 package org.dongwon;
 
 public class Calculator {
+
     public int calculate(int num1, int num2, char operation) {
         int result = 0;
         switch (operation) {
@@ -12,13 +13,15 @@ public class Calculator {
                 break;
             case '/':
                 if (num2 != 0) result = num1 / num2;
+                else throw new IllegalArgumentException("나눗셈 연산 시 두번째 숫자는 0이 아니어야 합니다.");
                 break;
             case '*':
                 result = num1 * num2;
                 break;
             default:
-                System.out.println("사칙연산 기호가 아닙니다.");
+                throw new IllegalArgumentException("사칙연산 기호가 아닙니다.");
         }
+
         return result;
     }
 }
